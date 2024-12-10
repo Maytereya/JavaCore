b0618167133acf63a22c57be9b88cdde1532173d
Vladimir Rakhmanov <69515014+Maytereya@users.noreply.github.com>
2024-12-10 17:08:06 +0400

    Create Main.java
    файл Main.java с простым примером кода

---
+++ Main.java
@@ -0,0 +1,16 @@
+package ru.gb;
+
+/**
+ * Основной класс приложения.
+ * В нем демонстрируется пример работы приложения.
+ */
+public class Main {
+    /**
+     * Точка входа программы.
+     *
+     * @param args стандартные аргументы командной строки
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
